30f554bcb6047e44e49dc3d35dc8a44c4d02ae92
miya <qhfl1096@naver.com>
2024-08-11 18:11:29 +0900

    다른 사람 db값 조회 시도시 errorType 이름 변경

--- src/main/java/com/wskang/trip/exception/ResourceNotFoundException.java
+++ src/main/java/com/wskang/trip/exception/ResourceNotFoundException.java
@@ -6,13 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 // DB 검색 시 값이 없는 경우 예외처리
-@Getter
 public class ResourceNotFoundException extends RuntimeException{
-
-    //private final String errorType;
-
     public ResourceNotFoundException(String message) {
         super(message);
-        //this.errorType = errorType;
     }
 }
